2ed72f0a672187eed0ebb0df2155289ef71108a9
Jason Kraft <developer.kraft@gmail.com>
2023-06-02 08:59:28 -0400

    Add Storybook support to project

---
+++ .storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from "@storybook/svelte-vite";
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/svelte-vite",
+    options: {},
+  },
+  core: {
+    disableTelemetry: true,
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
